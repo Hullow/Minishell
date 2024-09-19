7706222ae3db66fbbd09148c0972730615cdbdd3
C0X23 <megret.corentin@gmail.com>
2024-09-19 11:34:06 +0200

    Refactoriser les fichiers du projet et mettre à jour les commentaires dans main_cd.c et main_utils.c

--- Code/srcs/builtin/cd/main_cd.c
+++ Code/srcs/builtin/cd/main_cd.c
@@ -6,7 +6,7 @@
 /*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 15:00:25 by cmegret           #+#    #+#             */
-/*   Updated: 2024/09/11 15:45:59 by cmegret          ###   ########.fr       */
+/*   Updated: 2024/09/12 14:02:39 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ void	ft_cd(struct s_command *cmd, struct s_shell_state *shell_state)
 	shell_state->current_directory = getcwd(NULL, 0);
 	if (!shell_state->current_directory)
 		error_and_exit("getcwd failed");
-	setenv("OLDPWD", getenv("PWD"), 1);
-	setenv("PWD", shell_state->current_directory, 1);
+	setenv("OLDPWD", getenv("PWD"), 1); // Change later
+	setenv("PWD", shell_state->current_directory, 1); // Change later
 }
--- Code/srcs/main/main_utils.c
+++ Code/srcs/main/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 14:36:39 by cmegret           #+#    #+#             */
-/*   Updated: 2024/09/11 15:34:00 by cmegret          ###   ########.fr       */
+/*   Updated: 2024/09/12 15:19:41 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_initialize(int argc, char **argv, struct s_shell_state *shell_state)
 {
 	if (argc > 1)
 	{
-		fprintf(stderr, "Usage: %s\n", argv[0]);
-		fprintf(stderr, "No arguments are allowed\n");
+		printf("Usage: %s\n", argv[0]);
+		printf("No arguments are allowed\n");
 		exit(EXIT_FAILURE);
 	}
 	shell_state->current_directory = getcwd(NULL, 0);
