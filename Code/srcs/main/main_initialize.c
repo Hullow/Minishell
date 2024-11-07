138046d4e3dedebdc05f42057a0ded835bc70d70
C0X23 <megret.corentin@gmail.com>
2024-11-07 14:13:14 +0100

    Ajout de la documentation des fonctions dans main_initialize.c pour améliorer la lisibilité et la compréhension du code.

--- Code/srcs/main/main_initialize.c
+++ Code/srcs/main/main_initialize.c
@@ -6,12 +6,20 @@
 /*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 17:31:30 by cmegret           #+#    #+#             */
-/*   Updated: 2024/11/01 18:35:40 by cmegret          ###   ########.fr       */
+/*   Updated: 2024/11/07 13:59:58 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/Minishell.h"
-
+/**
+ * @brief Checks the command-line arguments.
+ *
+ * This function checks if any command-line arguments are provided.
+ * If arguments are provided, it prints a usage message and exits the program.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv An array of command-line arguments.
+ */
 static void	check_arguments(int argc, char **argv)
 {
 	if (argc > 1)
@@ -22,6 +30,15 @@ static void	check_arguments(int argc, char **argv)
 	}
 }
 
+/**
+ * @brief Counts the number of environment variables.
+ *
+ * This function counts the number of environment variables provided in the
+ * envp array.
+ *
+ * @param envp An array of environment variables.
+ * @return The number of environment variables.
+ */
 static int	count_env_variables(char **envp)
 {
 	int	count;
@@ -32,6 +49,16 @@ static int	count_env_variables(char **envp)
 	return (count);
 }
 
+/**
+ * @brief Duplicates the environment variables.
+ *
+ * This function duplicates the environment variables from the envp array
+ * into the shell_state structure.
+ *
+ * @param shell_state A pointer to the shell state structure.
+ * @param envp An array of environment variables.
+ * @param count The number of environment variables.
+ */
 static void	duplicate_env(t_shell_state *shell_state,
 	char **envp, int count)
 {
@@ -56,13 +83,25 @@ static void	duplicate_env(t_shell_state *shell_state,
 	shell_state->envp[count] = NULL;
 }
 
+/**
+ * @brief Initializes the shell state.
+ *
+ * This function initializes the shell state by setting up signal handlers,
+ * checking command-line arguments, counting environment variables, and
+ * duplicating the environment variables into the shell state structure.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv An array of command-line arguments.
+ * @param shell_state A pointer to the shell state structure.
+ * @param envp An array of environment variables.
+ */
 void	ft_initialize(int argc, char **argv,
 	t_shell_state *shell_state, char **envp)
 {
 	int	count;
 
-	signal(SIGINT, handle_sigint);// ctrl-C
-	signal(SIGQUIT, handle_sigquit);// 'ctrl-\'
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigquit);
 	check_arguments(argc, argv);
 	count = count_env_variables(envp);
 	duplicate_env(shell_state, envp, count);
