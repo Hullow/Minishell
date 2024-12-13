ffdf6e968f65e57b28bd0161134405eb2176d997
C0X23 <megret.corentin@gmail.com>
2024-12-14 00:28:09 +0100

    Heredocs: Add a main fonction for env variable il heredocs

--- Code/header/Heredocs.h
+++ Code/header/Heredocs.h
@@ -6,7 +6,7 @@
 /*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 22:40:32 by fallan            #+#    #+#             */
-/*   Updated: 2024/12/13 21:03:30 by cmegret          ###   ########.fr       */
+/*   Updated: 2024/12/13 21:31:10 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@
 typedef struct s_heredoc	t_heredoc;
 typedef struct s_token		t_token;
 
-void		ft_open_heredocs(t_command *cmd_list);
-void		ft_handle_heredoc_input(t_redir *redir_list);
+bool		ft_match_heredoc_delimiter(char *heredoc_line, char *delimiter);
 t_heredoc	*ft_init_heredoc(void);
 t_heredoc	*ft_malloc_new_heredoc_line(t_heredoc *heredoc_line);
-bool		ft_match_heredoc_delimiter(char *heredoc_line, char *delimiter);
+t_heredoc	*initialize_heredoc(t_redir *redir_list);
+bool		read_and_process_line(t_heredoc **heredoc_line,
+				t_redir *redir_list);
+void		ft_handle_heredoc_input(t_redir *redir_list);
+void		ft_open_heredocs(t_command *cmd_list);
 
 // Debugging
 void		ft_print_heredocs(t_command *cmd_list);
--- Code/srcs/heredocs/heredocs_main.c
+++ Code/srcs/heredocs/heredocs_main.c
@@ -3,42 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   heredocs_main.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: francis <francis@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cmegret <cmegret@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 17:27:25 by fallan            #+#    #+#             */
-/*   Updated: 2024/12/13 20:20:38 by francis          ###   ########.fr       */
+/*   Updated: 2024/12/13 21:51:04 by cmegret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/Minishell.h"
 
-// 2.7 Redirection
-//  Redirection is used to open and close files 
-//  for the current shell execution environment or for any command.
-
-	// 2.7.4 Here-Document
-// The redirection operators "<<" allows redirection 
-// of subsequent lines read by the shell 
-// to the input of a command.
-//
-// The redirected lines are known as a "here-document".
-// The here-document shall be treated as a single word 
-// that begins after the next NEWLINE token,
-//  and continues until there is a line containing only the delimiter 
-//  and a <newline>, with no <blank> characters in between.
-
-
-// matches a heredoc line (linked list of tokens, with strings inside)
-// with the heredoc delimiter (string)
+/**
+ * @brief Compare une ligne de heredoc avec le délimiteur.
+ *
+ * @param heredoc_line La ligne du heredoc à comparer.
+ * @param delimiter Le délimiteur du heredoc.
+ * @return true si les deux chaînes sont égales, false sinon.
+ */
 bool	ft_match_heredoc_delimiter(char *heredoc_line, char *delimiter)
 {
-	if (!ft_strncmp(heredoc_line, delimiter, ft_strlen(heredoc_line + 1))) // very basic version, for testing
+	size_t	len;
+
+	if (!heredoc_line || !delimiter)
+		return (false);
+	len = ft_strlen(heredoc_line);
+	if (!ft_strncmp(heredoc_line, delimiter, len))
 		return (true);
 	return (false);
 }
 
-// mallocs the first node of our heredoc
-// (which is a linked list of heredoc lines)
+/**
+ * @brief Initialise une nouvelle structure de heredoc.
+ *
+ * @return Un pointeur vers la nouvelle structure heredoc,
+ * ou NULL en cas d'erreur.
+ */
 t_heredoc	*ft_init_heredoc(void)
 {
 	t_heredoc	*heredoc_line;
@@ -51,88 +49,139 @@ t_heredoc	*ft_init_heredoc(void)
 	return (heredoc_line);
 }
 
-// mallocs a new heredoc line and sets heredoc_line address to it 
+/**
+ * @brief Alloue une nouvelle ligne de heredoc et l'ajoute à la liste.
+ *
+ * @param heredoc_line La ligne actuelle du heredoc.
+ * @return Un pointeur vers la nouvelle ligne de heredoc,
+ * ou NULL en cas d'erreur.
+ */
 t_heredoc	*ft_malloc_new_heredoc_line(t_heredoc *heredoc_line)
 {
 	heredoc_line->next = malloc(sizeof(t_heredoc));
-	heredoc_line = heredoc_line->next;
-	if (!heredoc_line)
+	if (!heredoc_line->next)
 		return (NULL);
+	heredoc_line = heredoc_line->next;
 	heredoc_line->line = NULL;
 	heredoc_line->next = NULL;
 	return (heredoc_line);
 }
 
-// Manages the input of one heredoc:
-// - mallocs a heredoc (i.e. a heredoc line list node)
-// - opens a heredoc prompt
-// - reads the input line by line
-// - split each line into an array of strings
-// - store the split of the line in a linked list node (t_heredoc),
-// 		 which is itself stored in the list of redirections (t_redir/redir_list)
-// - if a line split matches the heredoc delimiter, exit the prompt
+/**
+ * @brief Initialise et assigne la structure heredoc à la redirection.
+ *
+ * @param redir_list La redirection heredoc à traiter.
+ * @return t_heredoc* Un pointeur vers la structure heredoc
+ * initialisée, ou NULL en cas d'erreur.
+ */
+t_heredoc	*initialize_heredoc(t_redir *redir_list)
+{
+	t_heredoc	*heredoc_line;
+
+	heredoc_line = ft_init_heredoc();
+	if (!heredoc_line)
+		return (NULL);
+	redir_list->heredoc = heredoc_line;
+	return (heredoc_line);
+}
+
+/**
+ * @brief Lit une ligne de heredoc et la traite.
+ *
+ * @param heredoc_line Le pointeur vers la ligne actuelle de heredoc.
+ * @param redir_list La redirection heredoc à traiter.
+ * @return true si le délimiteur a été trouvé, false sinon.
+ */
+bool	read_and_process_line(t_heredoc **heredoc_line, t_redir *redir_list)
+{
+	char	*prompt;
+
+	prompt = ft_prompt(REDIR_HEREDOC);
+	if (!prompt)
+	{
+		printf("\n");
+		return (true);
+	}
+	if (*prompt)
+	{
+		(*heredoc_line)->line = ft_strdup(prompt);
+		if (ft_match_heredoc_delimiter((*heredoc_line)->line, redir_list->str))
+		{
+			free(prompt);
+			return (true);
+		}
+		*heredoc_line = ft_malloc_new_heredoc_line(*heredoc_line);
+		if (!(*heredoc_line))
+		{
+			free(prompt);
+			return (true);
+		}
+	}
+	free(prompt);
+	return (false);
+}
+
+/**
+ * @brief Gère l'entrée d'un heredoc.
+ *
+ * Cette fonction :
+ * - Initialise la structure de heredoc.
+ * - Ouvre le prompt heredoc.
+ * - Lit les lignes jusqu'au délimiteur.
+ * - Stocke les lignes dans la liste de heredoc.
+ *
+ * @param redir_list La redirection heredoc à traiter.
+ */
 void	ft_handle_heredoc_input(t_redir *redir_list)
 {
 	t_heredoc	*heredoc_line;
-	char		*prompt;
+	bool		delimiter_found;
 
 	if (!redir_list || !redir_list->str)
 		return ;
-	heredoc_line = ft_init_heredoc();
+	heredoc_line = initialize_heredoc(redir_list);
 	if (!heredoc_line)
 		return ;
-	redir_list->heredoc = heredoc_line;
 	while (1)
 	{
-		prompt = ft_prompt(REDIR_HEREDOC);
-		if (!prompt) // EOF (Ctrl+D)
-		{
-			printf("\n");
+		delimiter_found = read_and_process_line(&heredoc_line, redir_list);
+		if (delimiter_found)
 			break ;
-		}
-		if (*prompt)
-		{
-			heredoc_line->line = ft_strdup(prompt);
-			if (ft_match_heredoc_delimiter(heredoc_line->line, redir_list->str))
-			{
-				free(prompt);
-				break ;
-			}
-			heredoc_line = ft_malloc_new_heredoc_line(heredoc_line);
-			if (!heredoc_line)
-			{
-				free(prompt);
-				break ;
-			}
-		}
-		free(prompt);
 	}
 }
 
-// The main heredoc input function
-// goes through each command and the command's list of redirections
-// if a redirection is a heredoc, calls ft_handle_heredoc_input
+/**
+ * @brief Traite tous les heredocs dans la liste de commandes.
+ *
+ * Parcourt chaque commande et ses redirections pour gérer les heredocs.
+ *
+ * @param cmd_list La liste des commandes à traiter.
+ */
 void	ft_open_heredocs(t_command *cmd_list)
 {
 	t_redir		*redir_list;
-	// t_command	*cmd_list_head;
 
-	// cmd_list_head = cmd_list; // Save the head of the command list
 	while (cmd_list)
 	{
 		redir_list = cmd_list->redir_list;
 		while (redir_list)
 		{
-			if (redir_list->type == REDIR_HEREDOC && redir_list->str_type == WORD)
+			if (redir_list->type == REDIR_HEREDOC
+				&& redir_list->str_type == WORD)
 				ft_handle_heredoc_input(redir_list);
 			redir_list = redir_list->next;
 		}
 		cmd_list = cmd_list->next;
 	}
-	// ft_print_heredocs(cmd_list_head); // Appel de la fonction pour imprimer le contenu des heredocs
 }
 
-// prints all heredocs in all redirection lists for all commands
+/**
+ * @brief Affiche le contenu de tous les heredocs dans la liste de commandes.
+ *
+ * Pour chaque heredoc, affiche son contenu.
+ *
+ * @param cmd_list La liste des commandes contenant les heredocs.
+ */
 void	ft_print_heredocs(t_command *cmd_list)
 {
 	t_redir		*redir_list;
@@ -145,7 +194,8 @@ void	ft_print_heredocs(t_command *cmd_list)
 		redir_list = cmd_list->redir_list;
 		while (redir_list)
 		{
-			if (redir_list->type == REDIR_HEREDOC && redir_list->str_type == WORD)
+			if (redir_list->type == REDIR_HEREDOC
+				&& redir_list->str_type == WORD)
 			{
 				heredoc = redir_list->heredoc;
 				if (!heredoc)
@@ -156,13 +206,12 @@ void	ft_print_heredocs(t_command *cmd_list)
 				printf("Contenu du heredoc pour %s:\n", redir_list->str);
 				while (heredoc)
 				{
-					printf("{%s}", heredoc->line);
+					printf("{%s}\n", heredoc->line);
 					heredoc = heredoc->next;
-				}
-				printf("\n");
 				}
 			}
 			redir_list = redir_list->next;
 		}
 		cmd_list = cmd_list->next;
+	}
 }
