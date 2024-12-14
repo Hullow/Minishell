b334efe9d734db959243cce6a595603992611e1e
C0X23 <megret.corentin@gmail.com>
2024-12-14 13:47:25 +0100

    Heredocs: suppression des fichiers de test obsolètes pour simplifier la suite de tests

--- Test_Minishell/tests/test_03_path_variations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ./test_structure.sh
-
-run_test "/\nexit\n" "/: Is a directory" 126 "Root directory access"
-run_test "//\nexit\n" "//: Is a directory" 126 "Double root directory access"
-run_test "/.\nexit\n" "/.: Is a directory" 126 "Single dot directory"
-run_test "/./././././.\nexit\n" "/./././././.: Is a directory" 126 "Multiple dots directory"
-run_test "///////\nexit\n" "///////: Is a directory" 126 "Many slashes directory"
